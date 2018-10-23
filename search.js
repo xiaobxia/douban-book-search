3d13a4f92d236e05c39b2c4b5e80ca08d71c8efe
xiaobxia <673806687@qq.com>
2018-10-23 11:38:15 +0800

    xg

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const phantom = require('phantom');
 const fs = require('fs-extra');
 
-const searchText = encodeURI('竞争');
+const searchText = encodeURI('货币');
 //豆瓣是15
 const startStep = 15;
 
