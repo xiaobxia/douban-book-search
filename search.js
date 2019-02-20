06d5c6ab2247d88611b09f22b0b1941e2b77a165
xiaobxia <673806687@qq.com>
2019-02-20 11:16:36 +0800

    xg

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const phantom = require('phantom');
 const fs = require('fs-extra');
 
-const searchText = encodeURI('诗');
+const searchText = encodeURI('写作');
 //豆瓣是15
 const startStep = 15;
 
