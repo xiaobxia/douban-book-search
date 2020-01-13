1bedc79fdf1a7db3746ff0a48c4346f4973e89f6
xiaobxia <673806687@qq.com>
2020-01-13 11:50:24 +0800

    xg

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const phantom = require('phantom');
 const fs = require('fs-extra');
 
-const searchText = encodeURI('记忆');
+const searchText = encodeURI('EFT');
 //豆瓣是15
 const startStep = 15;
 
